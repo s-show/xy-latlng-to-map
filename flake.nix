2ae50d75287fc05a182c388dba8f463526809bfb
s-show <s1shimz@gmail.com>
2025-09-25 14:25:22 +0900

    Use Flakes.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            nodePackages.pnpm
+          ];
+          shellHook = ''
+            echo "Entering custom Nix shell..."
+          '';
+        };
+      }
+    );
+}
